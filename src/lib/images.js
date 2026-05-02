7efd8576588043c0cae04cb8b7068922326fb080
linsadry <adriana_lins@hotmail.com>
2026-05-02 15:43:27 -0300

    Create images.js

---
+++ src/lib/images.js
@@ -0,0 +1,13 @@
+const IMG = {
+  icon:       'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/icon.PNG',
+  cliffs:     'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/cliffs.PNG',
+  island:     'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/island.PNG',
+  abstract:   'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/abstract.PNG',
+  objects:    'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/objects.PNG',
+  map:        'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/map.PNG',
+  beach:      'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/beach.PNG',
+  fish:       'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/fish.PNG',
+  balloon:    'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/baloon.PNG',
+  lighthouse: 'https://uxkjvbjlsbgmbalokisf.supabase.co/storage/v1/object/public/rastro-images/lighthouse.PNG',
+}
+export default IMG
