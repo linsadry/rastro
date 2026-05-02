be1cd8607f883628e12166676580cfd8d40f43bd
linsadry <adriana_lins@hotmail.com>
2026-05-02 18:21:03 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,2 @@
-import { createClient } from '@supabase/supabase-js'
-
-const url = 'https://uxkjvbjlsbgmbalokisf.supabase.co'
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4a2p2Ympsc2JnbWJhbG9raXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MDU3NjMsImV4cCI6MjA0OTA4MTc2M30.eOtAl-n3qNSLR0BQNKhr8jiE5qXResibjKVut0fpEHQ'
-
-export const sb = createClient(url, key)
+import{createClient}from'@supabase/supabase-js'
+export const sb=createClient('https://uxkjvbjlsbgmbalokisf.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4a2p2Ympsc2JnbWJhbG9raXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1MDU3NjMsImV4cCI6MjA0OTA4MTc2M30.eOtAl-n3qNSLR0BQNKhr8jiE5qXResibjKVut0fpEHQ')
